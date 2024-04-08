7523dc14a7b1c651dda002c870eef6d0a2a4ab5b
Harsh <hgaur491@gmail.com>
2024-04-08 14:50:56 +0530

    creating simple note app

--- App.tsx
+++ App.tsx
@@ -5,76 +5,20 @@
  * @format
  */
 
+import Home from "@src/screens/home/Screen";
 import React from "react";
-import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
-
-type CardProps = {
-    bg: string;
-    children?: React.ReactNode;
-};
-
-function Card(props: CardProps) {
-    const { bg, children } = props;
-    return (
-        <View className={`items-center justify-center w-32 h-32 rounded-md m-2 ${bg}`}>
-            <Text className="text-white">{children}</Text>
-        </View>
-    );
-}
+import { SafeAreaView, ScrollView } from "react-native";
 
 export default function App() {
     return (
-        <SafeAreaView>
-            <ScrollView contentInsetAdjustmentBehavior="automatic">
-                <Text className="text-3xl">FlatCard</Text>
-                <ScrollView
-                        horizontal={true}
-                        contentInsetAdjustmentBehavior="automatic"
-                        className="bg-neutral-700 "
-                    >
-                    <Card bg="bg-red-600">Text</Card>
-                    <Card bg="bg-blue-600">Text</Card>
-                    <Card bg="bg-green-600">Text</Card>
-                    <Card bg="bg-orange-600">Text</Card>
-                </ScrollView>
-                <View>
-                    <Image
-                        className="w-full h-60"
-                        source={{
-                            uri: "https://c4.wallpaperflare.com/wallpaper/974/565/254/windows-11-windows-10-minimalism-hd-wallpaper-preview.jpg",
-                        }}
-                    />
-                </View>
+        <SafeAreaView className="h-full">
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic" 
+                className="h-full bg-stone-900"
+                >
+                <Home />
             </ScrollView>
+            
         </SafeAreaView>
     );
 }
-// export default function App() {
-
-//     return (
-//         <SafeAreaView className="h-full pb-12">
-//             <ScrollView
-//                 contentInsetAdjustmentBehavior="automatic"
-//                 className="h-full"
-//                 >
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//                 <Home />
-//             </ScrollView>
-//             <Navbar />
-//         </SafeAreaView>
-//     );
-// }
--- src/screens/home/Screen.tsx
+++ src/screens/home/Screen.tsx
@@ -1,10 +1,123 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image, TextInput, Button } from "react-native";
+import React, { useState } from "react";
 
-export default function Screen() {
+type NoteProps = {
+    title: string;
+    note: string;
+    id: number;
+    handleDeleteNote: (id: number) => void;
+};
+function Note(props:NoteProps) {
+    const { title, note, id } = props;
     return (
-        <View>
-            <Text>Screen</Text>
+        <View className="border border-blue-500 bg-blue-600 p-2 w-full mt-2 rounded-md">
+            <View>
+            <Text className="text-xl">{title}</Text>
+            <Text className="text-sm">{note}</Text>
+            </View>
+            <Text onPress={() => props.handleDeleteNote(id)} className="text-red-500 font-bold text-2xl absolute right-2 top-2">X</Text>
         </View>
+    );
+}
+
+const noteData = [
+    {
+        id: 1,
+        title: "Note 1",
+        note: "this my note",
+    },
+    {
+        id: 2,
+        title: "Note 2",
+        note: "this my note",
+    },
+    // {
+    //     id: 3,
+    //     title: "Note 3",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 4,
+    //     title: "Note 4",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 5,
+    //     title: "Note 5",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 6,
+    //     title: "Note 6",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 7,
+    //     title: "Note 7",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 8,
+    //     title: "Note 8",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 9,
+    //     title: "Note 9",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 10,
+    //     title: "Note 10",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 11,
+    //     title: "Note 11",
+    //     note: "this my note",
+    // },
+    // {
+    //     id: 12,
+    //     title: "Note 12",
+    //     note: "this my note",
+    // },
+];
+export default function Home() {
+    const [notes, setNotes] = useState(noteData);
+    const [noteInput, setNoteInput] = useState("");
+    const handleAddNote = () => {
+        if (noteInput) {
+            const newNote = {
+                id: notes.length + 1,
+                title: noteInput,
+                note: noteInput,
+            };
+            setNotes([...notes, newNote]);
+            setNoteInput("");
+        }
+    }
+    const handleDeleteNote = (id: number) => {
+        const newNotes = notes.filter((note) => note.id !== id);
+        setNotes(newNotes);
+    }
+    return (
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <View className=" w-full bg-blue-500 p-1 h-12 flex-row items-center justify-between">
+                <TextInput
+                    placeholder="Add Notes"
+                    value={noteInput}
+                    onChangeText={setNoteInput}
+                    className="bg-blue-700 w-full rounded-md h-10 px-2 text-white"
+                    onSubmitEditing={handleAddNote}
+                />
+                {/* <Button title="Add Note" onPress={handleAddNote} /> */}
+            </View>
+            <Text className="text-3xl font-bold">Notes</Text>
+            <View className="p-2">
+                {notes.map((note) => (
+                    <Note key={note.id} title={note.title} note={note.note} id={note.id} handleDeleteNote={handleDeleteNote}/>
+                ))}
+            </View>
+        </ScrollView>
     );
 }
